6df901a05861f483bec49e97c8f0023e35e07a0f
csontosreka <61116319+csontosreka@users.noreply.github.com>
2021-10-08 18:05:38 +0200

    Added database connection

--- login.php
+++ login.php
@@ -3,14 +3,12 @@
         <title>Favourite color</title>
     </head>
     <body>
-        <?php
-            echo '<form action ="processing.php" method="POST">
-                    Username: <br>
-                    <input type="text" name="u_name"> <br>
-                    Password: <br>
-                    <input type ="password" name="pwd"><br>
-                    <input type="submit" value="Sign in">
-                </form>';
-        ?>
+        <form action ="processing.php" method="POST">
+            Username: <br>
+            <input type="text" name="u_name"> <br>
+            Password: <br>
+            <input type ="password" name="pwd"><br>
+            <input type="submit" value="Sign in">
+        </form>
     </body>
 </html>
--- processing.php
+++ processing.php
@@ -1,48 +1,83 @@
 <?php
+    $givenUsername = $_POST['u_name'];
+    $givenPassword = $_POST['pwd'];
+
     $file = fopen("password.txt", "r") or die("No such file or directory");
     $wholefile=fread($file, filesize("password.txt"));  // beolvasom a teljes txt állományt
     //echo "$wholefile";
     fclose($file);
 
-    $counter = 1;
-    $decoded_pwd = "";
+    $decodedPasswords = decodePasswordFile($wholefile);
 
-    for ($ch = 0; $ch <= filesize("password.txt"); $ch++) {
-        $ascii = substr($wholefile, $ch, 1); //a teljes fájl adott karaktere
-        $decimal = ord($ascii); //ascii karakter átváltása decimálisra
-        //echo $ch . ": " . $decimal . "<br>";
+    $command = "SELECT username, titkos FROM `tabla` WHERE `username` like '$givenUsername';";
+    $datas = connectToDatabase($command);
+    foreach ($datas as ["username" => $username, "titkos" => $szin]) {
+        echo "username: $username, szin: $szin\n";
+    }
 
-        if ($decimal == 10){
-            $decoded_pwd .= "|"; //ha elválasztó karaktert találok ezt a karaktert fűzöm a dekódolt stringhez
-            $counter = 1;
-        } else {
-            switch ($counter) {
-                case 1:
-                    $counter++;
-                    $decimal = $decimal - 5;
-                    break;
-                case 2:
-                    $counter++;
-                    $decimal = $decimal + 14;
-                    break;
-                case 3:
-                    $counter++;
-                    $decimal = $decimal - 31;
-                    break;
-                case 4:
-                    $counter++;
-                    $decimal = $decimal + 9;
-                    break;
-                case 5:
-                    $counter = 1;
-                    $decimal = $decimal - 3;
-                    break;
+    function decodePasswordFile($pwdfile){
+
+        $counter = 1;
+        $decoded_pwd = "";
+
+        for ($ch = 0; $ch <= strlen($pwdfile); $ch++) {
+            $ascii = substr($pwdfile, $ch, 1); //a teljes fájl adott karaktere
+            $decimal = ord($ascii); //ascii karakter átváltása decimálisra
+            //echo $ch . ": " . $decimal . "<br>";
+
+            if ($decimal == 10){
+                $decoded_pwd .= "|"; //ha elválasztó karaktert találok ezt a karaktert fűzöm a dekódolt stringhez
+                $counter = 1;
+            } else {
+                switch ($counter) {
+                    case 1:
+                        $counter++;
+                        $decimal = $decimal - 5;
+                        break;
+                    case 2:
+                        $counter++;
+                        $decimal = $decimal + 14;
+                        break;
+                    case 3:
+                        $counter++;
+                        $decimal = $decimal - 31;
+                        break;
+                    case 4:
+                        $counter++;
+                        $decimal = $decimal + 9;
+                        break;
+                    case 5:
+                        $counter = 1;
+                        $decimal = $decimal - 3;
+                        break;
+                }
             }
+
+            $decoded_ascii = chr($decimal); // visszaváltás decimálisról ascii-re
+            $decoded_pwd .= $decoded_ascii; // hozzáfűzés a dekódolt szöveget tartalmazó stringhez
         }
 
-        $decoded_ascii = chr($decimal); // visszaváltás decimálisról ascii-re
-        $decoded_pwd .= $decoded_ascii; // hozzáfűzés a dekódolt szöveget tartalmazó stringhez
-      }
+        return $decoded_pwd;
+    }
 
-      echo $decoded_pwd;
+    function connectToDatabase($command){
+        
+        $conn = new mysqli("localhost", "root", "", "adatok");
+		$result = mysqli_query($conn, $command);
+        if (!$result) { 
+            die("Query Failed."); 
+        }
+
+        $rows = array();
+        if (mysqli_num_rows($result) > 0){
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $rows[] = $row;
+            }
+        }
+        mysqli_close($conn);
+        
+        return $rows;
+    }
+    
 ?>
